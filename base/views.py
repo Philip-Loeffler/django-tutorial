85ada40edb3c4d1e6b806757caa08806151cd453
philip-loeffler <philiploeffler@gmail.com>
2021-12-06 14:36:19 -0500

    passing dict

--- base/views.py
+++ base/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+rooms = {
+    {'id': 1, 'name': 'lets learn python'},
+    {'id': 2, 'name': 'design with me'},
+    {'id': 3, 'name': 'front end developer'},
+}
 # Create your views here.
+# {'rooms': rooms}) Adding this allows us that room object to be accessable inside our home template
 def home(request):
-    return render(request, 'home.html')
+    # can also do it like this
+    # context = {'rooms': rooms}
+    # then pass context into the render function
+    return render(request, 'home.html', {'rooms': rooms})
 
 def room(request):
     return render(request, 'room.html')
