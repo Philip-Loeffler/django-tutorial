8115b269acd9aba7076fb847f7401eb8367362c2
philip-loeffler <philiploeffler@gmail.com>
2021-12-07 10:26:21 -0500

    dyanmic routing

--- base/views.py
+++ base/views.py
@@ -1,19 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
-
-rooms = {
+rooms = [
     {'id': 1, 'name': 'lets learn python'},
     {'id': 2, 'name': 'design with me'},
     {'id': 3, 'name': 'front end developer'},
-}
+]
 # Create your views here.
 # {'rooms': rooms}) Adding this allows us that room object to be accessable inside our home template
 def home(request):
-    # can also do it like this
-    # context = {'rooms': rooms}
-    # then pass context into the render function
-    return render(request, 'home.html', {'rooms': rooms})
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return render(request, 'room.html')
+
+def room(request, pk):
+    return render(request, 'base/room.html')
